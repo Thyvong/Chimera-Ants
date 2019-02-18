92be8639051f6371f22e6a97269a3f272c749f5d
Thyvong <kenthyvong.tan@gmail.com>
2019-02-18 11:18:23 +0100

    Instantiate trees and rocks on terrain

--- Chimera-Ants/Assets/scripts/environement/zone/MapsGenerator.cs
+++ Chimera-Ants/Assets/scripts/environement/zone/MapsGenerator.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MapsGenerator: MonoBehaviour
 {
+    /* Paramètre de génération du terrain*/
     public float lacunarity, persistence, scale, heightmeshscale;
     public int octaves, width, height;
     public Biome[] biomes;
     public AnimationCurve heightCorrectionCurve;
+
+    /* Données générés */
+    float[,] _heightMap;
+    Color[] _colorMap;
+    Texture2D _texture;
+    Mesh _mesh;
     
+
+    /* Paramètre de génération du décor */
+    public GameObject treeModel;
+    public Transform treeList; // object parent de tous les trees à placer ici
+    List<int> treesLocationIndex = new List<int>();
+    public GameObject rockModel;
+    public Transform rockList; // object parent de tous les cailloux à placer ici
+    List<int> rocksLocationIndex = new List<int>();
+
+
+    /* Methode */
     public float[,] generateHeightMap()
     {
         float[,] map = new float[width, height];
@@ -75,7 +94,6 @@ public class MapsGenerator: MonoBehaviour
         return colormap;
     }
 
-    
     public Mesh generateMesh(float[,] heightmap)
     {
         int width = heightmap.GetLength(0);
@@ -103,24 +121,111 @@ public class MapsGenerator: MonoBehaviour
         terrain.uv = data.uvs;
         terrain.triangles = data.triangles;
         terrain.RecalculateNormals();
-
+        
         return terrain;
+    }
+
+    public void GenerateRocks(float[,] heightmap, Mesh mesh)
+    {
+        System.Random rng = new System.Random();
+        int nbRocks = rng.Next();
+
+        int plainIndex = 0;
+        for (int it = 0; it < biomes.Length; it++)
+        {
+            if (biomes[it].zoneType == TerritoryType.PLAINE)
+            {
+                plainIndex = it;
+                break;
+            }
+            else
+            {
+                plainIndex = -1;
+            }
+        }
+        if (plainIndex == -1)
+            return;
+        
+        
+        int index = rng.Next() % mesh.vertexCount;
+
+        for (int i = 0; i < nbRocks; i++)
+        {
+            do
+            {
+                index = rng.Next() % mesh.vertexCount;
+            } while (
+                (heightmap[index / width, index % width] > biomes[plainIndex].height
+                || heightmap[index / width, index % width] < biomes[plainIndex--].height)
+                ||
+                treesLocationIndex.Contains(index)
+                || rocksLocationIndex.Contains(index)
+            );
+            // save pos as an index in an array. Don't forget to check if pos is not already occupied
+            rocksLocationIndex.Add(index);
+            Instantiate(rockModel, mesh.vertices[index], Quaternion.FromToRotation(Vector3.up, mesh.tangents[index]), rockList);
+        }
+
+
+    }
+    public void GenerateVegetation(float[,] heightmap, Mesh mesh)
+    {
+        System.Random rng = new System.Random();
+        int nbTrees = rng.Next();
+
+        int plainIndex=0;
+        for(int it = 0; it < biomes.Length; it++)
+        {
+            if(biomes[it].zoneType == TerritoryType.PLAINE)
+            {
+                plainIndex = it;
+                break;
+            }
+            else
+            {
+                plainIndex = -1;
+            }
+        }
+        if (plainIndex == -1)
+            return;
+
+        
+        int index = rng.Next()%mesh.vertexCount;
+        
+        for (int i = 0; i < nbTrees; i++)
+        {
+            do
+            {
+                index = rng.Next() % mesh.vertexCount;
+            } while (
+                (heightmap[index / width, index % width] > biomes[plainIndex].height
+                || heightmap[index / width, index % width] < biomes[plainIndex--].height)
+                ||
+                treesLocationIndex.Contains(index)
+                ||rocksLocationIndex.Contains(index)
+            );
+            // save pos as an index in an array. Don't forget to check if pos is not already occupied
+            treesLocationIndex.Add(index);
+            Instantiate(treeModel, mesh.vertices[index], Quaternion.FromToRotation(Vector3.up, mesh.tangents[index]), treeList);
+        }
+        
     }
 
     public void CreateMap()
     {
         MapRenderer maprenderer = gameObject.GetComponent<MapRenderer>();
-        float[,] heightmap = generateHeightMap();
-        Color[] colormap = generateColorMap(heightmap);
-        Texture2D texture = maprenderer.generateTexture(colormap, width, height);
-        Mesh mesh = generateMesh(heightmap);
+        _heightMap = generateHeightMap();
+        _colorMap = generateColorMap(_heightMap);
+        _texture = maprenderer.generateTexture(_colorMap, width, height);
+        _mesh = generateMesh(_heightMap);
 
-        maprenderer.renderMesh(mesh, texture);
-        maprenderer.meshfilter.gameObject.GetComponent<MeshCollider>().sharedMesh = mesh;
+        maprenderer.renderMesh(_mesh, _texture);
+        maprenderer.meshfilter.gameObject.GetComponent<MeshCollider>().sharedMesh = _mesh;
     }
     
 
 }
+/* Classe support à la création de mesh */
 public class MeshData
 {
     public Vector3[] vertices;
